d429fad83dc01346ef17ffc5c17f2c748452b57d
MateusHBR <mateusmorais.r@gmail.com>
2021-06-19 12:36:13 -0300

    ensure http adapter throws Unauthorized on 401

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -146,6 +146,18 @@ void main() {
 
       expect(future, throwsA(HttpError.badRequest));
     });
+
+    test('should return Unauthorized if post returns 401', () async {
+      mockResponse(statusCode: 401);
+
+      final future = sut.request(
+        url: url,
+        method: 'post',
+      );
+
+      expect(future, throwsA(HttpError.unauthorized));
+    });
+
     test('should return ServerError if post returns 500', () async {
       mockResponse(statusCode: 500);
 
