b37ebe10aad9e2e1b0ad54ebacdbd72cc3f92411
MateusHBR <mateusmorais.r@gmail.com>
2021-06-19 12:38:53 -0300

    ensure http adapter throws Forbidden on 403

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -158,6 +158,17 @@ void main() {
       expect(future, throwsA(HttpError.unauthorized));
     });
 
+    test('should return forbidden if post returns 403', () async {
+      mockResponse(statusCode: 403);
+
+      final future = sut.request(
+        url: url,
+        method: 'post',
+      );
+
+      expect(future, throwsA(HttpError.forbidden));
+    });
+
     test('should return ServerError if post returns 500', () async {
       mockResponse(statusCode: 500);
 
