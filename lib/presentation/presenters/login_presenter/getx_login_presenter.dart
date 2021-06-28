73ec84ef541ae2a3cb895af5a157a81e20912a39
MateusHBR <mateusmorais.r@gmail.com>
2021-06-28 13:07:23 -0300

    remove hide loading in success case

--- lib/presentation/presenters/login_presenter/getx_login_presenter.dart
+++ lib/presentation/presenters/login_presenter/getx_login_presenter.dart
@@ -94,7 +94,6 @@ class GetXLoginPresenter extends GetxController implements LoginPresenter {
     } on DomainError catch (error) {
       _errorObserver.value = error.description;
       _errorObserver.value = null;
-    } finally {
       _loadingObserver.value = false;
     }
   }
