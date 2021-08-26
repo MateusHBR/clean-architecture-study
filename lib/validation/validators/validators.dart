d2789768c70473bbec15654a7451eb84054576dc
MateusHBR <mateusmorais.r@gmail.com>
2021-08-26 20:17:17 -0300

    add to exports file

--- lib/validation/validators/validators.dart
+++ lib/validation/validators/validators.dart
@@ -2,3 +2,5 @@ export 'validation_composite.dart';
 
 export 'required_field_validation.dart';
 export 'email_field_validation.dart';
+export 'compare_field_validation.dart';
+export 'min_length_validation.dart';
