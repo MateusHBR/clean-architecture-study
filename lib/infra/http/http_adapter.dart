2c5f3423b8fcc2f292efc92295fb1aaefa2c2edc
MateusHBR <mateusmorais.r@gmail.com>
2021-09-08 23:34:15 -0300

    add timeout to http post

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -29,11 +29,15 @@ class HttpAdapter implements HttpClient {
 
     try {
       if (method == 'post') {
-        response = await client.post(
-          Uri.parse(url),
-          headers: defaultHeaders,
-          body: jsonBody,
-        );
+        response = await client
+            .post(
+              Uri.parse(url),
+              headers: defaultHeaders,
+              body: jsonBody,
+            )
+            .timeout(
+              Duration(seconds: 10),
+            );
       } else if (method == 'get') {
         response = await client.get(
           Uri.parse(url),
