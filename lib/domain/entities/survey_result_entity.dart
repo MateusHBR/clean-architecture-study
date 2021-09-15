decc4a5bcdbb61aef37d68b578258561aa20abc4
MateusHBR <mateusmorais.r@gmail.com>
2021-09-15 20:55:53 -0300

    add survey result entity

---
+++ lib/domain/entities/survey_result_entity.dart
@@ -0,0 +1,24 @@
+import 'package:course_clean_arch/domain/entities/entities.dart';
+import 'package:equatable/equatable.dart';
+
+class SurveyResultEntity extends Equatable {
+  final String surveyId;
+  final String question;
+  final bool didAnswer;
+  final List<SurveyAnswerEntity> answers;
+
+  SurveyResultEntity({
+    required this.surveyId,
+    required this.question,
+    required this.didAnswer,
+    required this.answers,
+  });
+
+  @override
+  List<Object> get props => [
+        surveyId,
+        question,
+        didAnswer,
+        answers,
+      ];
+}
